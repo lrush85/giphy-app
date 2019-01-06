44da44aa76d6b22548013acb930cbdcc3bee3ba4
lrush85 <lance.rush1@gmail.com>
2019-01-05 19:44:43 -0600

    Added images for app, created additional logic for changing gif amount, added side bar and styles

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,19 +1,33 @@
 
 $(document).ready(function() {
-
-
-
-    var topics = ["michael jordan", "spiderman", "jonah hill", "superman", "batman", "jenna fischer"];
+    $(".toggle-sidebar").on("click", function(){
+        $("#sidebar").toggleClass("collapsed");
+        $("#content").toggleClass("col-md-12 col-md-9");
+    });
     
+    
+    var topics = ["mario bros", "excitebike", "duck hunt", "galaga"];
+
+    // Initial value
+    var limit = 10;
+
+    $(".add").change( function () {
+        console.log($(this).val());
+    });
+    
+
 
     // This function is to set up gif display
 
     function displayGiphyInfo () {
 
         var topic = $(this).attr("data-name");
-        var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=lUSK1fPW7gLS0BbQoVE7ZsfLOk0e3tja&q=" + topic + "&limit=10&offset=0&rating=G&lang=en";
+        console.log(topic);
 
-        
+        limit = $(".add").val();
+
+        var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=lUSK1fPW7gLS0BbQoVE7ZsfLOk0e3tja&q=" + topic + "&limit=" + limit + "&offset=0&rating=&lang=en";
+
         $.ajax({
             url: queryURL,
             method: "GET"
@@ -21,37 +35,38 @@ $(document).ready(function() {
             
             result = response.data;
             
-                for(var i = 0; i < result.length; i++) {
-                    var topicDiv = $("<div class='topic-div col-md-4 col-sm-6'>");
-                    // Set up rating and holder of that rating.
-                    var rating = result[i].rating;
-                    
-                    // Image animate set up
-                    var imageAnimate = result[i].images.fixed_height.url;
-                    
-                    // Image still set up
-                    var imageStill = result[i].images.fixed_height_still.url;
+            for(var i = 0; i < result.length; i++) {
+                var topicDiv = $("<div class='topic-div col-md-4 img-fluid'>");
+                // Set up rating and holder of that rating.
+                var rating = result[i].rating;
+                
+                // Image animate set up
+                var imageAnimate = result[i].images.fixed_height.url;
+                
+                // Image still set up
+                var imageStill = result[i].images.fixed_height_still.url;
 
-                    // Image set up
-                    
-                    var imageGif = $("<img>");
-                    var p = $("<p>").text("Rating: " + rating);
-                    
-                    imageGif.attr("src", imageStill);
-                    imageGif.addClass("gif");
-                    imageGif.attr("data-state", "still");
-                    imageGif.attr("data-still", imageStill);
-                    imageGif.attr("data-animate", imageAnimate);
+                // Image set up
+                
+                var imageGif = $("<img>");
+                var p = $("<span>").text(rating);
+                
+                imageGif.attr("src", imageStill);
+                imageGif.addClass("gif");
+                imageGif.attr("data-state", "still");
+                imageGif.attr("data-still", imageStill);
+                imageGif.attr("data-animate", imageAnimate);
 
-                    topicDiv.append(imageGif);
-                    topicDiv.append(p);
-                    $("#gifs").prepend(topicDiv);
-                    
-                }
-
+                topicDiv.append(imageGif);
+                topicDiv.append(p);
+                $("#gifs").append(topicDiv);
+            }
+            
         });
-        
+        $("#gifs").empty();
     }
+    
+
 
     function showButtons () {
 
@@ -60,16 +75,14 @@ $(document).ready(function() {
 
         for (var i = 0; i < topics.length; i++) {
             
-            
             var topicButton = $("<button>");
-            topicButton.addClass("topic btn col-md-2");
+            topicButton.addClass("topic btn col-sm-12 col-md-2");
             topicButton.attr("data-name", topics[i]);
             topicButton.text(topics[i]);
             $("#buttons-view").append(topicButton);
 
-            
-        }
-        
+        }     
+        $("#gifs").empty();
     }
 
     $("#add-topic").on("click", function(event){
@@ -82,6 +95,7 @@ $(document).ready(function() {
 
         showButtons();
     });
+
 
     $(document).on("click", ".topic", displayGiphyInfo);
     $(document).on("click", ".gif", pauseGif);
@@ -102,3 +116,4 @@ $(document).ready(function() {
     }
 
 });
+
